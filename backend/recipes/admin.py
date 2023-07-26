dd80cc3b9ee69b0121e0c205ef3e3b332bd785dc
sof07 <64215063+sof07@users.noreply.github.com>
2023-07-26 23:53:34 +0300

    Создана модель ингридиентов рецептов, зарегестрированы в админке

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from recipes.models import Recipes, Ingridients
+
+
+class IngridientsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'author', 'title', 'description',
+                    'date', 'cooking_time', 'ingridients')
+    empty_value_display = '-пусто-'
+    list_editable = ('name', 'year', 'category')
+
+
+admin.site.register(Recipes)
+admin.site.register(Ingridients)
+admin.site.register(IngridientsAdmin)
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Ingridients(models.Model):
+    title = models.CharField('Название ингридиента', max_length=250)
+    quantity = models.DecimalField('Количество', max_length=1000)
+    unit_measurement = models.CharField('Единица измерения', max_length=100)
+
+class Recipes(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes')
+    title = models.CharField('Название рецепта', max_length=250)
+    description = models.TextField('Описание рецепта')
+    image = models.ImageField(
+        upload_to='recipes/images/', 
+        null=True,  
+        default=None
+        )
+    date = models.DateField('Дата создания', auto_now_add=True)
+    cooking_time = models.DecimalField('Время приготовления мин.' max_length=4) #Подумать над типом поля
+    ingridients = models.ManyToManyField(
+        Ingridients,
+        related_name='recipes',
+    )
+    #tag =
