2f8b1dca52801b84253dadbcc0c2a577f647d22f
sof07 <sof07@yandex.ru>
2023-09-29 00:00:08 +0300

    Исправлены импорты

--- backend/recipes/management/commands/csvloader.py
+++ backend/recipes/management/commands/csvloader.py
@@ -1,7 +1,8 @@
-from django.core.management.base import BaseCommand
-from recipes.models import Ingredient
 import csv
 import os
+
+from django.core.management.base import BaseCommand
+from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -9,7 +9,6 @@ from rest_framework import filters, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
-
 from users.models import AuthorSubscription, CustomUser
 
 from .filters import RecipeFilter
