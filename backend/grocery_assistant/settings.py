0fe1ed405362238e19111df9fc6851ffbb5dc4d2
sof07 <sof07@yandex.ru>
2023-09-29 22:08:02 +0300

    Токен

--- backend/grocery_assistant/settings.py
+++ backend/grocery_assistant/settings.py
@@ -1,9 +1,6 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
-
-load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('TOKEN', default="23GG")
+SECRET_KEY = '%zxtgft^6^nk3zdonq5*er+1l6(g(@2vf*wqbc_f#&k@+-+27i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
