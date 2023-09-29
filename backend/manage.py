62042d61a6f0c2d24c87f604f06baf97d4c562ed
sof07 <sof07@yandex.ru>
2023-09-30 01:29:56 +0300

    manage.py pep8

--- backend/manage.py
+++ backend/manage.py
@@ -5,7 +5,10 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grocery_assistant.settings')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE',
+        'grocery_assistant.settings'
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
