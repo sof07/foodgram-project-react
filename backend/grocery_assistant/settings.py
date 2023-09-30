97af69d9bc9ebfd81960450975bf9d0df92dbc30
sof07 <sof07@yandex.ru>
2023-09-30 22:30:55 +0300

    Спрятал ключи в энв

--- backend/grocery_assistant/settings.py
+++ backend/grocery_assistant/settings.py
@@ -1,8 +1,17 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.getenv('TOKEN', default="23GG")
+
+DEBUG = False
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
