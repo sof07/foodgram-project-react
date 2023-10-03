1fff98d9d64bba786b9b12feb7a3446831ef8354
sof07 <sof07@yandex.ru>
2023-10-03 22:03:18 +0300

    flake 8

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -7,8 +7,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
 from rest_framework import filters, permissions, status, viewsets
 from rest_framework.decorators import action
-from rest_framework.pagination import (LimitOffsetPagination,
-                                       PageNumberPagination)
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from users.models import AuthorSubscription, CustomUser
 
