801c9315b8ff48a256d2bcdbc58bb6ca39441be6
sof07 <sof07@yandex.ru>
2023-10-03 11:09:25 +0300

    сломал урлы

--- backend/grocery_assistant/settings.py
+++ backend/grocery_assistant/settings.py
@@ -13,13 +13,13 @@ load_dotenv()
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
 
-BASE_DIR = 'namr'
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECRET_KEY = '%zxtgft^6^nk3zdonq5*er+1l6(g(@2vf*wqbc_f#&k@+-+27i'
+SECRET_KEY = '%zxtgft^6^nk3zdonq5*er+1l6(g(@2vf*wqbc_f#&k@+-+27i'
 
-# DEBUG = True
+DEBUG = True
 
-# ALLOWED_HOSTS = ['84.252.137.104', '127.0.0.1', 'localhost', 'sof07.ddns.net']
+ALLOWED_HOSTS = ['84.252.137.104', '127.0.0.1', 'localhost', 'sof07.ddns.net']
 
 
 INSTALLED_APPS = [
