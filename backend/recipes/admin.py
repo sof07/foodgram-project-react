2075993f8cb8761ad16ab3e30b99ea4d36e21cae
sof07 <sof07@yandex.ru>
2023-10-05 23:09:17 +0300

    убраны миниатюры

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.utils.safestring import mark_safe
 
 from .models import (Favorite, Ingredient, IngredientRecipe, Recipe,
                      ShoppingCart, Tag)
@@ -37,18 +36,13 @@ class IngredientAdmin(admin.ModelAdmin):
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('name', 'author', 'cooking_time',
-                    'get_html_photo', 'get_favorite_count')
+                    'image', 'get_favorite_count')
     list_filter = ('author', 'tags', 'ingredients')
     search_fields = ('name', 'author__username')
-
-    def get_html_photo(self, object):
-        if object.image:
-            return mark_safe(f"<img src='{object.image.url}'eidth=50")
 
     def get_favorite_count(self, obj):
         return Favorite.objects.filter(recipe=obj).count()
 
     get_favorite_count.short_description = 'Добавлено в избранное'
-    get_html_photo.short_description = 'Изображение'
 
     inlines = [IngredientRecipeInline]
