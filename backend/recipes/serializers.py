fe33ff22b64a7a7855bdcedb86828cec4c03924a
sof07 <sof07@yandex.ru>
2023-10-22 22:34:19 +0300

    убран base 64

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -39,15 +39,15 @@ class TagSerializer(serializers.ModelSerializer):
         read_only_fields = ['name', 'color', 'slug']
 
 
-class Base64ImageField(serializers.ImageField):
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
+# class Base64ImageField(serializers.ImageField):
+#     def to_internal_value(self, data):
+#         if isinstance(data, str) and data.startswith('data:image'):
+#             format, imgstr = data.split(';base64,')
+#             ext = format.split('/')[-1]
 
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+#             data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
 
-        return super().to_internal_value(data)
+#         return super().to_internal_value(data)
 
 
 class IngredientRecipeSerializer(serializers.ModelSerializer):
